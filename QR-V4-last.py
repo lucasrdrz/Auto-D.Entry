6d1340caa95904390ffe6a5d3b4975f60fe47416
Lucas Rodriguez <110471982+lucasrdrz@users.noreply.github.com>
2026-01-22 09:09:12 -0300

    Update QR-V4-last.py

--- QR-V4-last.py
+++ QR-V4-last.py
@@ -92,7 +92,7 @@ if uploaded_file is not None:
         st.session_state.df = df
 
         st.subheader('Datos del archivo Excel')
-        st.dataframe(df)
+        st.write(pd.DataFrame(df))
 
         st.download_button(
             label="Descargar archivo",
@@ -159,6 +159,7 @@ if uploaded_file is not None:
                     ).execute()
             else:
                 st.write(f"Fila {i + 1}: datos incompletos, omitida.")
+
 
 
 
